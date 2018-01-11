dd7d1b33cabf8ffaf2a7fdb7236c4c71f0e3bb44
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2018-01-11 16:50:24 +0100

    preperation for submitting this to thesis advisor

--- _build/build.sh
+++
@@ -1,4 +0,0 @@
-lualatex --file-line-error --synctex=1 --shell-escape --output-directory=.build main.tex
-makeindex -q -s .build/main.ist -o .build/main.acr -t .build/main.alg .build/main.acn
-makeindex -q -s .build/main.ist -o .build/main.gls -t .build/main.glg .build/main.glo
-lualatex --file-line-error --synctex=1 --shell-escape --output-directory=.build main.tex
